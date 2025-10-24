68ad3dc49841a53dcc97d5a5c8fff8a97e947f3f
Antônio Della Flora <antoniodellaflora@gmail.com>
2025-10-24 09:25:50 -0300

    feat: :sparkles: add app configuration for Android and update app name
    - Created app.config.js to manage Android-specific configurations, including Google Services JSON file path.
    - Updated app.json to change the app name to "Lembrete Anticoncepcional".

---
+++ app.config.js
@@ -0,0 +1,16 @@
+export default {
+  android: {
+    googleServicesFile:
+      process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
+  },
+  expo: {
+    extra: {
+      eas: {
+        projectId: "d5aa500e-4157-4f25-908c-8c7bf12a406d",
+      },
+    },
+    android: {
+      package: "com.dfantonio.lembreteanticoncepcional",
+    },
+  },
+};
